36574290c47a6badffc7b9ac80b6596ebe20d6d6
dinasalsabilla <dinasalsabilla05@gmail.com>
2025-03-17 00:35:38 +0700

    menambahkan class DataGeneric

--- tpmodul5_103022300153/Program.cs
+++ tpmodul5_103022300153/Program.cs
@@ -8,12 +8,29 @@
         }
     }
 
+    class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {data}");
+        }
+    }
     class Program
     {
         static void Main()
         {
             HaloGeneric halo = new HaloGeneric();
             halo.SapaUser("Dina");
+
+            DataGeneric<string> dataNIM = new DataGeneric<string>("103022300153");
+            dataNIM.PrintData();
         }
     }
 }
